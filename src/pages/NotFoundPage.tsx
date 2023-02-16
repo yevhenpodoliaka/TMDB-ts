f4b8fe4b6d824a2cbc548910cb277e5645e2dcd2
Yevhen Podoliaka <zanp85740@gmail.com>
2023-02-16 14:18:58 +0200

    fix 404 page add 404 in public

--- src/pages/NotFoundPage.tsx
+++ src/pages/NotFoundPage.tsx
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom"
 
 const  NotFoundPage=() =>{
     return <div className="notFoundPage">
-        <h1>404  NotFound !!!</h1>
+        <h1>404 Page Not Found !!!</h1>
         <Link to={'/'} >Go Home Page</Link>
     </div>
 }
