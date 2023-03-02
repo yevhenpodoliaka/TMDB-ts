f149b068e1de3f0ee4e7129af4e8518188866b63
Yevhen Podoliaka <zanp85740@gmail.com>
2023-03-02 21:30:25 +0200

    added  netlify scripts

--- src/index.tsx
+++ src/index.tsx
@@ -10,7 +10,7 @@ const root = ReactDOM.createRoot(
 );
 root.render(
   <React.StrictMode>
-    <BrowserRouter basename='TMDB-ts'>
+    <BrowserRouter>
       <App/>
       </BrowserRouter>
    </React.StrictMode>
